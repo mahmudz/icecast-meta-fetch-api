1a492533fe77599923a7d708cecab9371e7315a8
Mahmudul Hasan <mahmudbappy.pri@gmail.com>
2022-06-27 10:56:41 +0600

    Fixes

--- code/meta-api/controllers/fetchController.py
+++ code/meta-api/controllers/fetchController.py
@@ -8,27 +8,32 @@ import urllib.request as urllib2
 
 def fetch_meta(url):
     encoding = 'utf-8'  # default: iso-8859-1 for mp3 and utf-8 for ogg streams
-    request = urllib2.Request(
-        url, headers={'Icy-MetaData': 1}
-    )  # request metadata
+    try:
+        request = urllib2.Request(
+            url, headers={'Icy-MetaData': 1}
+        )  # request metadata
 
-    response = urllib2.urlopen(request)
-    metaint = int(response.headers['icy-metaint'])
+        response = urllib2.urlopen(request)
+        metaint = int(response.headers['icy-metaint'])
 
-    for _ in range(10):  # title may be empty initially, try several times
-        response.read(metaint)  # skip to metadata
-        metadata_length = struct.unpack('B', response.read(1))[
-            0] * 16  # length byte
+        for _ in range(10):  # title may be empty initially, try several times
+            response.read(metaint)  # skip to metadata
+            metadata_length = struct.unpack('B', response.read(1))[
+                0] * 16  # length byte
 
-        metadata = response.read(metadata_length).rstrip(b'\0')
-        m = re.search(br"StreamTitle='([^']*)';", metadata)
-        if m:
-            title = m.group(1)
-            if title:
-                break
-    else:
+            metadata = response.read(metadata_length).rstrip(b'\0')
+            print(metadata.decode(encoding))
+            m = re.search(br"StreamTitle='([^']*)';", metadata)
+            if m:
+                title = m.group(1)
+                if title:
+                    break
+        else:
+            return {}
+
+        return {
+            'title': title.decode(encoding, 'replace')
+        }
+    except Exception as e:
+        print('Failed to fetch metadata:', e)
         return {}
-
-    return {
-        'title': title.decode(encoding, 'replace')
-    }
